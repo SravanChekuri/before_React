79ab93f5f637fbccdaa1cf70aac126c04b7e75ae
Sravan Varma <sravanchekuri449@gmail.com>
2026-07-27 20:30:41 +0530

    Remove CDN react script and download parcel, react, react-dom from npm

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 console.log("=====================================================");
 console.log("Learning React js");
 console.log("=====================================================");
