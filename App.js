716e25ddac01f12b2dadaf8821e12dd3a54bb197
Sravan Varma <sravanchekuri449@gmail.com>
2026-07-27 18:54:42 +0530

    Getting React from CDN links, and understand basics

---
+++ App.js
@@ -0,0 +1,20 @@
+console.log("=====================================================");
+console.log("Learning React js");
+console.log("=====================================================");
+console.log(React);
+console.log(ReactDOM);
+console.log("=====================================================");
+
+const greetingEle = React.createElement(
+  "h2",
+  { id: "greet" },
+  "Welcome to React World!",
+);
+console.log(greetingEle);
+
+const renderEle = ReactDOM.createRoot(document.querySelector("#root"));
+console.log(renderEle);
+
+console.log("=====================================================");
+
+renderEle.render(greetingEle);
---
+++ README.md
@@ -0,0 +1,3 @@
+# This repository is for learing purpose
+
+## Learning Ract js
